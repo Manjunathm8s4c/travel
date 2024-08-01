622e4bcc0bcc6775dc0b406399ebc3fa6a82bb0b
Manjunathm8s4c <craftempt@gmail.com>
2024-08-01 12:16:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple form validation (for demonstration purposes)
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting us! We will get back to you soon.');
+});
